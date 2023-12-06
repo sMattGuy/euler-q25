63acd312f3bc1f7d1ff3b3d4d87b170879a373b8
Matthew <matthewflammia@aol.com>
2023-12-06 14:34:24 +0000

    Upload of code for solving problem

---
+++ q25.js
@@ -0,0 +1,17 @@
+let target = 1000;
+let f1 = BigInt(1);
+let f2 = BigInt(1);
+let current = BigInt(0);
+let iteration = 2;
+
+while(current.toString().length != target){
+	iteration++;
+	current = f1+f2;
+	f1 = f2;
+	f2 = current;
+	console.log(iteration);
+	if(iteration > 4782){
+		console.log(`failed! f1:${f1} f2:${f2} current:${current}`);
+		return;
+	}
+}
